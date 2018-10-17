93427b5f69b8a49acb9949bfbf4168d725904049
John P Cavalieri <jcavalieri8619@gmail.com>
2018-10-17 17:26:00 -0400

    init commit

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/ui/SoundClickCallback.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.beatbox.ui;
+
+
+import com.bignerdranch.android.beatbox.model.Sound;
+
+public interface SoundClickCallback {
+
+    void onClick(Sound sound);
+
+}
